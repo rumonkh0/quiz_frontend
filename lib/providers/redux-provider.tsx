b6f5dc1ae68059a0f64e8288a4eb6e6c7033620c
rumonkh0 <rummankh0@gmail.com>
2025-04-21 22:20:38 +0600

    update 2

--- lib/providers/redux-provider.tsx
+++ lib/providers/redux-provider.tsx
@@ -1,12 +1,12 @@
 // lib/providers/redux-provider.tsx
-'use client';
+"use client";
 
-import { ReactNode, useRef } from 'react';
-import { Provider } from 'react-redux';
-import { AppStore, makeStore } from '@/store/store';
+import { ReactNode, useRef } from "react";
+import { Provider } from "react-redux";
+import { AppStore, makeStore } from "@/store/store";
 
 export function ReduxProvider({ children }: { children: ReactNode }) {
-  const storeRef = useRef<AppStore>();
+  const storeRef = useRef<AppStore | null>(null);
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = makeStore();
